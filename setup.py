9631fd0eb5b737dc5c02bde5bfa380d3993ccc6c
kek <d.li@ira-labs.com>
2023-08-15 15:35:15 +0300

    fisrts iteration

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', encoding='utf-8') as file:
+    requirements = file.read().splitlines()
+
+with open('README.md', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='diffusion4med',
+    version='0.0.0',
+    description='vox2vec: Self-Superivsed Learning of Voxel-level Representations in Medical Images',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/David-cripto/diffusion4med',
+    packages=find_packages(include=('diffusion4med',)),
+    python_requires='>=3.11',
+    install_requires=requirements,
+)
