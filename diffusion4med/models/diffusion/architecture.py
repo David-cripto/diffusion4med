b6deb841f7e8fbdbe8df0075fb09e306edb0919c
kek <d.li@ira-labs.com>
2023-08-16 15:28:28 +0300

    formatter

--- diffusion4med/models/diffusion/architecture.py
+++ diffusion4med/models/diffusion/architecture.py
@@ -1,9 +1,12 @@
 from torch import nn
+from torch import Tensor
 
 class Unet(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, channels = (1, 2, 4, 8)) -> None:
+        super().__init__()
         self.downpath = nn.ModuleList([])
         self.uppath = nn.ModuleList([])
         
         
+    def forward(self, image: Tensor):
+        pass
